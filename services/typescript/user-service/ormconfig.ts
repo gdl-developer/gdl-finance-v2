b4e3953a6600562b30c34afdd6edba56ef191886
gdl-developer <developer@gdl.com.ng>
2026-05-12 13:23:22 +0100

    feat: migrate all services in GDL-Fintech-V2 to MySQL and update infrastructure configuration

--- services/typescript/user-service/ormconfig.ts
+++ services/typescript/user-service/ormconfig.ts
@@ -5,7 +5,7 @@ import { ConnectionOptions } from 'typeorm';
 const config = new EnvService().read();
 
 const connectionOptions: ConnectionOptions = {
-  type: (process.env.DB_TYPE as any) || 'postgres',
+  type: (process.env.DB_TYPE as any) || 'mysql',
   host: config.DB_HOST,
   port: config.DB_PORT,
   username: config.DB_USERNAME,
