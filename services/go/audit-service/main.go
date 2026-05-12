bd1bb4e6a5e92e42b796ee75c4b78212d331dda1
gdl-developer <developer@gdl.com.ng>
2026-05-12 11:59:17 +0100

    test: disable sslmode to debug database connection refusal

--- services/go/audit-service/main.go
+++ services/go/audit-service/main.go
@@ -48,7 +48,7 @@ func main() {
 	dbPass := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		dbHost, dbUser, dbPass, dbName, dbPort)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
